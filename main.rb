62ca8c24ea7fba239def28d541edb362909ee7ce
Felipe Pardo Suarez <felipe.pardosuarez@gmail.com>
2017-02-14 03:52:53 +0000

    Initial commit

---
+++ main.rb
@@ -0,0 +1,20 @@
+#Classes:
+  #Game:
+    #vars: current_player, players, curr_answer, curr_question
+    #methods: game_loop, output_lives, decrease_live
+  #Players:
+    #vars: name, lives
+    #methods: alive?
+  #Questions/answers:
+    #methods: get_rand_quesiton, ask_question, correct_answer?
+
+# require './Plarers.rb'
+
+class Main
+
+  def game_loop
+
+  end
+
+end
+
